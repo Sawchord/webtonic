9bea1779a18fe167446315683604a61d6cad5da8
Leon Tan <leon.arian.tan@gmail.com>
2020-12-02 01:21:13 +0100

    Remove WebTonic Future and use simple call instead

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,17 @@ mod response;
 
 use core::{
     fmt,
-    future::Future,
     marker::PhantomData,
-    pin::Pin,
     task::{Context, Poll},
 };
 use futures::future::{FutureExt, LocalBoxFuture};
 use http::{request::Request, response::Response, uri::Uri};
 use std::error::Error;
 use tonic::{body::BoxBody, client::GrpcService};
-use wasm_bindgen::JsValue;
+//use wasm_bindgen::JsValue;
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{console, window, Request as JsRequest};
+use web_sys::window;
+//use web_sys::console;
 
 use crate::{request::req_to_js_req, response::js_res_to_res};
 
@@ -36,7 +35,7 @@ impl WebTonic<'_> {
 impl<'a> GrpcService<BoxBody> for WebTonic<'a> {
     type ResponseBody = BoxBody;
     type Error = WebTonicError;
-    type Future = WebTonicFuture<'a>;
+    type Future = LocalBoxFuture<'a, Result<Response<BoxBody>, WebTonicError>>;
 
     fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         // We return an ok, because we are essentially always ready to poll
@@ -45,7 +44,7 @@ impl<'a> GrpcService<BoxBody> for WebTonic<'a> {
 
     fn call(&mut self, request: Request<BoxBody>) -> Self::Future {
         let uri_clone = self.uri.clone();
-        WebTonicFuture::Request((async { req_to_js_req(uri_clone, request).await }).boxed_local())
+        (async { call(uri_clone, request).await }).boxed_local()
     }
 }
 
@@ -62,55 +61,10 @@ impl fmt::Display for WebTonicError {
     }
 }
 
-// TODO: Add inner to hide the internals
-pub enum WebTonicFuture<'a> {
-    Request(LocalBoxFuture<'a, Result<JsRequest, WebTonicError>>),
-    Fetch(JsFuture),
-    Response(LocalBoxFuture<'a, Result<Response<BoxBody>, WebTonicError>>),
-}
-impl<'a> Future for WebTonicFuture<'a> {
-    type Output = Result<
-        Response<<WebTonic<'a> as GrpcService<BoxBody>>::ResponseBody>,
-        <WebTonic<'a> as GrpcService<BoxBody>>::Error,
-    >;
-
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        console::log_1(&JsValue::from_str(&"polling"));
-        let inner = self.get_mut();
-        match inner {
-            WebTonicFuture::Request(future) => match future.poll_unpin(cx) {
-                Poll::Pending => Poll::Pending,
-                Poll::Ready(request) => match request {
-                    Err(err) => Poll::Ready(Err(err)),
-                    Ok(request) => {
-                        // Do the fetch
-                        console::log_1(&JsValue::from_str(&"fetching"));
-                        let fetch = JsFuture::from(window().unwrap().fetch_with_request(&request));
-                        *inner = WebTonicFuture::Fetch(fetch);
-                        Poll::Pending
-                    }
-                },
-            },
-            WebTonicFuture::Fetch(future) => match future.poll_unpin(cx) {
-                Poll::Pending => Poll::Pending,
-                Poll::Ready(response) => {
-                    console::log_1(&JsValue::from_str(&"parsing response"));
-                    let fut = js_res_to_res(response);
-                    *inner = WebTonicFuture::Response((async { fut.await }).boxed_local());
-                    Poll::Pending
-                }
-            },
-            WebTonicFuture::Response(future) => match future.poll_unpin(cx) {
-                Poll::Pending => Poll::Pending,
-                Poll::Ready(response) => {
-                    console::log_1(&JsValue::from_str(&"fetching"));
-                    Poll::Ready(response)
-                }
-            },
-            #[allow(unreachable_patterns)]
-            _ => todo!(),
-        }
-    }
+async fn call(uri: Uri, request: Request<BoxBody>) -> Result<Response<BoxBody>, WebTonicError> {
+    let request = req_to_js_req(uri, request).await?;
+    let response = JsFuture::from(window().unwrap().fetch_with_request(&request)).await;
+    js_res_to_res(response).await
 }
 
 #[cfg(test)]
