0f0f57e9c093c401a8a1df265b9cf01fce4052d4
Leon Tan <leon.arian.tan@gmail.com>
2020-12-02 20:07:40 +0100

    Implement WebSocket adapter

--- webtonic-client/src/lib.rs
+++ webtonic-client/src/lib.rs
@@ -1,6 +1,13 @@
 #![allow(dead_code, unused_imports)]
 
+use wasm_bindgen::JsValue;
+use web_sys::console;
+
 mod websocket;
+
+pub(crate) fn console_log(s: &str) {
+    console::log_1(&JsValue::from_str(s));
+}
 
 #[cfg(test)]
 mod tests {
--- webtonic-client/src/websocket.rs
+++ webtonic-client/src/websocket.rs
@@ -1,9 +1,12 @@
-use js_sys::{Function, Promise};
+use bytes::Bytes;
+use js_sys::{ArrayBuffer, Function, Promise, Uint8Array};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{console, ErrorEvent, MessageEvent, WebSocket};
 use webtonic_proto::WebTonicError;
+
+use crate::console_log;
 
 pub(crate) struct WebSocketConnector {
     ws: WebSocket,
@@ -12,6 +15,7 @@ pub(crate) struct WebSocketConnector {
 impl WebSocketConnector {
     pub(crate) async fn connect(uri: String) -> Result<Self, WebTonicError> {
         let ws = WebSocket::new(&uri).map_err(|_| WebTonicError::InvalidUrl)?;
+        unset_message_handler(&ws);
 
         // NOTE: We can only process ArrayBuffers at the moment
         ws.set_binary_type(web_sys::BinaryType::Arraybuffer);
@@ -19,10 +23,7 @@ impl WebSocketConnector {
         let connect_promise = Promise::new(&mut |resolve, error| {
             // Error callback
             let onerror_callback = Closure::wrap(Box::new(move |e: ErrorEvent| {
-                console::log_1(&JsValue::from_str(&format!(
-                    "Error while connecting {:?}",
-                    e
-                )));
+                console_log(&format!("Error while connecting {:?}", e));
                 error.call0(&JsValue::NULL).unwrap();
             }) as Box<dyn FnMut(ErrorEvent)>);
             ws.set_onerror(Some(onerror_callback.as_ref().unchecked_ref()));
@@ -30,7 +31,7 @@ impl WebSocketConnector {
 
             // Connect callback
             let onopen_callback = Closure::wrap(Box::new(move |_| {
-                console::log_1(&JsValue::from_str(&"Connected"));
+                console_log(&"Connected");
                 resolve.call0(&JsValue::NULL).unwrap();
             }) as Box<dyn FnMut(JsValue)>);
             ws.set_onopen(Some(onopen_callback.as_ref().unchecked_ref()));
@@ -43,7 +44,59 @@ impl WebSocketConnector {
         Ok(Self { ws })
     }
 
-    pub(crate) async fn send(&self) {
-        todo!()
+    pub(crate) async fn send(&self, request: &mut Bytes) -> Result<Bytes, WebTonicError> {
+        let ws = self.ws.clone();
+
+        let send_promise = Promise::new(&mut |resolve, error| {
+            // Error callback
+            let onerror_callback = Closure::wrap(Box::new(move |e: ErrorEvent| {
+                console_log(&format!("Error while connecting {:?}", e));
+                error.call0(&JsValue::NULL).unwrap();
+            }) as Box<dyn FnMut(ErrorEvent)>);
+            ws.set_onerror(Some(onerror_callback.as_ref().unchecked_ref()));
+            onerror_callback.forget();
+
+            let ws_clone = ws.clone();
+            // Message Callback
+            let onmessage_callback = Closure::wrap(Box::new(move |e: MessageEvent| {
+                // We only want to receive one message, therefore we unset the message handler here
+                unset_message_handler(&ws_clone);
+
+                if let Ok(buf) = e.data().dyn_into::<ArrayBuffer>() {
+                    resolve.call1(&JsValue::NULL, &JsValue::from(buf)).unwrap();
+                } else {
+                    console_log(&"Received unexpected message");
+                }
+            }) as Box<dyn FnMut(MessageEvent)>);
+            ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
+            onmessage_callback.forget();
+        });
+
+        // Send the actual data to the server
+        match self.ws.send_with_u8_array(request) {
+            Ok(_) => (),
+            Err(e) => {
+                console_log(&format!("Failed to send request {:?}", e));
+                return Err(WebTonicError::ConnectionError);
+            }
+        }
+
+        // Wait for message and parse it
+        let buf = JsFuture::from(send_promise)
+            .await
+            .map_err(|_| WebTonicError::ConnectionError)?;
+        let array = Uint8Array::new(&buf);
+        let body = Bytes::from(array.to_vec());
+
+        Ok(body)
     }
+}
+
+fn unset_message_handler(ws: &WebSocket) {
+    // Message Callback
+    let onmessage_callback = Closure::wrap(Box::new(move |_e: MessageEvent| {
+        console_log(&"Received unexpected message");
+    }) as Box<dyn FnMut(MessageEvent)>);
+    ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
+    onmessage_callback.forget();
 }
