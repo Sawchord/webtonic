bff0b7dcd5b5ac0bb952ad57552c7ad3d7aa73de
Leon Tan <leon.arian.tan@gmail.com>
2020-12-02 23:54:53 +0100

    WIP implement Server

--- webtonic-server/src/lib.rs
+++ webtonic-server/src/lib.rs
@@ -1,15 +1,23 @@
+use bytes::Bytes;
 use core::marker::{Send, Sync};
 use futures::StreamExt;
 use http::request::Request;
+use prost::Message as ProstMessage;
 use std::net::SocketAddr;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+use tokio::sync::Mutex;
 use tonic::body::BoxBody;
 use tower_service::Service;
-use warp::{ws::WebSocket, Filter};
+use warp::{
+    ws::{Message, WebSocket},
+    Filter,
+};
+use webtonic_proto::Call;
 
-//use core::future::Future;
-//use core::pin::Pin;
-//use std::error::Error;
+// use core::future::Future;
+// use core::pin::Pin;
+// use http::response::Response;
+// use std::error::Error;
 // type FullService = Box<
 //     dyn Service<
 //         Request<BoxBody>,
@@ -31,28 +39,64 @@ impl<B> Clone for Server<B> {
     }
 }
 
-impl<B: Service<Request<BoxBody>> + Sync + Send> Server<B> {
+impl<B: Service<Request<BoxBody>> + Sync + Send + 'static> Server<B> {
     pub fn build(service: B) -> Self {
         Self(Arc::new(Mutex::new(service)))
     }
 
-    pub async fn serve<A: Into<SocketAddr>>(self, addr: A) -> Result<(), ()> {
-        warp::serve(
-            warp::path::end()
-                .and(warp::ws())
-                .map(|ws: warp::ws::Ws| ws.on_upgrade(|socket| handle_connection(socket))),
-        )
+    pub async fn serve<A: Into<SocketAddr>>(self, addr: A) -> Result<(), ()>
+    where
+        <B as Service<Request<BoxBody>>>::Future: Send,
+    {
+        let server_clone = warp::any().map(move || self.clone());
+
+        warp::serve(warp::path::end().and(warp::ws()).and(server_clone).map(
+            |ws: warp::ws::Ws, server_clone| {
+                ws.on_upgrade(|socket| handle_connection(socket, server_clone))
+            },
+        ))
         .run(addr)
         .await;
 
         Ok(())
     }
 }
-async fn handle_connection(
-    //async fn handle_connection<B: Service<Request<BoxBody>> + Sync + Send>(
+//async fn handle_connection(
+async fn handle_connection<B: Service<Request<BoxBody>> + Sync + Send + 'static>(
     ws: WebSocket,
-    //server: &Server<B>,
+    server: Server<B>,
 ) {
-    let (_ws_tx, _ws_rx) = ws.split();
-    todo!()
+    let (ws_tx, mut ws_rx) = ws.split();
+
+    while let Some(msg) = ws_rx.next().await {
+        // Check that we got a message and it is binary
+        let msg = match msg {
+            Ok(msg) => {
+                if msg.is_binary() {
+                    Bytes::from(msg.into_bytes())
+                } else {
+                    todo!()
+                }
+            }
+            Err(_) => todo!(),
+        };
+
+        // Parse message first into protobuf then into http request
+        let call = match Call::decode(msg) {
+            Ok(call) => call,
+            Err(_) => todo!(),
+        };
+        let call = webtonic_proto::call_to_http_request(call).unwrap();
+
+        // Call the inner service
+        let response = match server.0.lock().await.call(call).await {
+            Ok(response) => response,
+            Err(_) => todo!(),
+        };
+
+        // Turn reply first into protobuf, then into message
+        //let reply = webtonic_proto::http_response_to_reply(&mut response).await;
+
+        // Return the message
+    }
 }
